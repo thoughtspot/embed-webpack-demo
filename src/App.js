721952c29946d1048ec62af555e70b4aa54fc272
Ashish Shubham <ashubham@gmail.com>
2023-06-21 12:05:37 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { init, AppEmbed, AuthType } from '@thoughtspot/visual-embed-sdk/react';
+
+init({
+  thoughtSpotHost: 'https://embed-1-do-not-delete.thoughtspotdev.cloud',
+  authType: AuthType.None,
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>This is TS Embed</div>
+      <AppEmbed className='ts-embed' frameParams={{height: '100%', width: '100%'}}></AppEmbed>
     </div>
   );
 }
